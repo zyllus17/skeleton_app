97e8658675941df485fad4eea614ca4da2828109
MarufHassanPietech <maruf@pietechsolution.com>
2024-02-17 17:25:10 +0530

    Created logic file for settings

---
+++ lib/pages/settings/settings.logic.dart
@@ -0,0 +1,11 @@
+import 'package:skeleton_app/db/db.dart';
+
+// Future<void> saveUserLanguagePreference() async {
+//   final db = ref.watch(dbProvider);
+//   db.saveAppLanguage(languageSelected: state.selectedLanguage);
+// }
+
+// void changeLanguage({required bool value}) {
+//   state = state.copyWith(selectedLanguage: value);
+//   saveUserLanguagePreference();
+// }
